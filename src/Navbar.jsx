9e702bdb912219e5386173b50070d10930e91e3a
clucksupremacy <cherrycluckcluck@gmail.com>
2026-05-13 17:35:41 -0700

    upload logo img and set size

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,14 +1,18 @@
 function Navbar() {
-
   return (
     <>
-     <div style={{ backgroundColor: '#eee', padding: '12px 16px' }}>
+     {/* <div style={{ backgroundColor: '#eee', padding: '12px 16px' }}>
         <span style={{ color: '#74ac00', fontWeight: 'bold', fontSize: '18px' }}>
           iNaturalist
         </span>
-      </div>
-      <nav>
-        
+      </div> */}
+      <nav style={{ backgroundColor: '#eee', padding: '12px 16px' }}>
+        <div className="logonav">
+            <img src="src/assets/logo.svg" style={{ width: '121px', height: '22px' }} />
+        </div>
+        <div className="searchnav">searchnav</div>
+        <div className="mainnav">mainnav</div>
+        <div className="usernav">usernav</div>
       </nav>
     </>
   )
