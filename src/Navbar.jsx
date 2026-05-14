c33292089f89cefe1d268f38d35049e1044de02e
clucksupremacy <cherrycluckcluck@gmail.com>
2026-05-13 18:55:44 -0700

    add fontawesome hamburger menu icon for mobile menu

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,11 +1,6 @@
 function Navbar() {
   return (
     <>
-     {/* <div style={{ backgroundColor: '#eee', padding: '12px 16px' }}>
-        <span style={{ color: '#74ac00', fontWeight: 'bold', fontSize: '18px' }}>
-          iNaturalist
-        </span>
-      </div> */}
       <nav style={{ backgroundColor: '#eee', padding: '12px 16px' }}>
         <div className="logonav">
             <img src="src/assets/logo.svg" style={{ width: '121px', height: '22px' }} />
@@ -13,6 +8,9 @@ function Navbar() {
         <div className="searchnav">searchnav</div>
         <div className="mainnav">mainnav</div>
         <div className="usernav">usernav</div>
+        <div>
+            <i className="fa-solid fa-bars" style={{ color: 'rgb(0, 0, 0)' }}></i>
+        </div>
       </nav>
     </>
   )
