e2ec44003fa5aadd71a75cf75f10b7711cc4d2c8
clucksupremacy <cherrycluckcluck@gmail.com>
2026-05-13 20:39:45 -0700

    add nav groups and search icon css

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,3 +1,5 @@
+import './Navbar.css'
+
 function Navbar() {
   return (
     <>
@@ -5,11 +7,15 @@ function Navbar() {
         <div className="logonav">
             <img src="src/assets/logo.svg" style={{ width: '121px', height: '22px' }} />
         </div>
-        <div className="searchnav">searchnav</div>
+        <div className="searchnav">
+            <button className="btn-search">
+                <i className="fa fa-search" />
+            </button>
+        </div>
         <div className="mainnav">mainnav</div>
         <div className="usernav">usernav</div>
         <div>
-            <i className="fa-solid fa-bars" style={{ color: 'rgb(0, 0, 0)' }}></i>
+            <i className="fa-solid fa-bars" style={{ color: 'rgb(0, 0, 0)' }} />
         </div>
       </nav>
     </>
