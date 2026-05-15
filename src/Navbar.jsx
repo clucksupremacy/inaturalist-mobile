fa5c7796f8802aa9d16c0340f85b7face98f5dcf
clucksupremacy <cherrycluckcluck@gmail.com>
2026-05-15 15:39:46 -0700

    toggle feature for nav links

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,25 +1,36 @@
+import { useState } from 'react'
 import './Navbar.css'
 
 function Navbar() {
-  return (
-    <>
-      <nav style={{ backgroundColor: '#eee', padding: '12px 16px' }}>
-        <div className="logonav">
-            <img src="src/assets/logo.svg" style={{ width: '121px', height: '22px' }} />
-        </div>
-        <div className="searchnav">
-            <button className="btn-search">
-                <i className="fa fa-search" />
-            </button>
-        </div>
-        <div className="mainnav">mainnav</div>
-        <div className="usernav">usernav</div>
-        <div>
-            <i className="fa-solid fa-bars" style={{ color: 'rgb(0, 0, 0)' }} />
-        </div>
-      </nav>
-    </>
-  )
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <>
+        <nav style={{ backgroundColor: '#eee', padding: '12px 16px' }}>
+            <div className="logonav">
+                <img src="src/assets/logo.svg" style={{ width: '121px', height: '22px' }} />
+            </div>
+            <div className="searchnav">
+                <button className="btn-search">
+                    <i className="fa fa-search" />
+                </button>
+            </div>
+            <div className={ isOpen ? "navlinks open" : "navlinks" }>
+                <ul>
+                    <li className="mainnav" onClick={toggleMenu}>mainnav</li>
+                    <li className="usernav" onClick={toggleMenu}>usernav</li>
+                </ul>
+            </div>
+            <div className="hamburger" onClick={toggleMenu}>
+                <i className="fa-solid fa-bars" style={{ color: 'rgb(0, 0, 0)' }} />
+            </div>
+        </nav>
+        </>
+    )
 }
 
 export default Navbar
